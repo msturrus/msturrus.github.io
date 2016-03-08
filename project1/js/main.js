59227d43bb6cd8c69037bc83fac2db30be69ce32
Michael Sturrus <msturrus@gmail.com>
2016-03-08 10:20:48 -0600

    added win/lose conditions and associated js

--- project1/js/main.js
+++ project1/js/main.js
@@ -1,6 +1,24 @@
+var doorCounter = 0;
+
 
 $('.doors:not(.rightDoor)').on('click', function() {
-	$(this).addClass("rightDoor");
+	self = this;
+	if (doorCounter === 20){
+		var youWin = $('<div>You Win!</div>');
+		$(self).addClass("lastDoor");
+		$(youWin).addClass('gotCaught');
+		$('body').append(youWin);
+	} else if (Math.floor(Math.random()* 5) === 1){
+		var gotCaught = $('<div>Ya got caught, son!</div>');
+		$(self).addClass("wrongDoor");
+		$(gotCaught).addClass('gotCaught');
+		$('body').append(gotCaught);
+		$('main').addClass('preposterous');
+	}
+		else {
+		$(self).addClass('rightDoor');
+		doorCounter++;
+	}
 
 
 });
